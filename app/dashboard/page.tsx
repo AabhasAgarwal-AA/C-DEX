0e0e818ca4a3ed6ce1a78551479006801d05885d
AabhasAgarwal-AA <aabhasagarwal50@gmail.com>
2026-03-18 13:16:19 +0000

    dashboard added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,34 @@
-"use client";
-import { useSession } from "next-auth/react"
+import { ProfileCard } from "@/components/ProfileCard";
+import db from "@/app/db";
+import { authConfig } from "../lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function() {
-    const session = useSession();
-    return <div>
-        Dashboard 
-    </div>
+async function getUserBalance(){
+    const session = await getServerSession(authConfig);
+    const userWallet = await db.solWallet.findFirst({
+        where: {
+            userId: session?.user?.uid 
+        }, 
+        select: {
+            publicKey: true
+        }
+    }); 
+    if(!userWallet){
+        return {
+            error: "No Solana wallet found associated to the user"
+        }
+    }
+    return {error: null, userWallet}; 
+}
+
+export default async function() {
+    const userWallet = await getUserBalance();
+    if(userWallet.error){
+        return <div>
+            No Solana wallet found
+        </div>
+    }
+   return <div>
+        <ProfileCard />
+   </div>
 }
